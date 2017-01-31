d52c3b52fc7a3aba979818e70a6b7a993fc77823
Ben Jay <fakebenjay@gmail.com>
2017-01-31 19:03:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_hash = {}
+  pigeons_array = []
+
+  data.each do |category, attributes|
+    if category == :color
+      attributes.each do |stat, pigeons|
+        pigeons.each do |name|
+          pigeons_array << name
+          pigeons_array.uniq!
+        end
+      end
+    end
+  end
+
+  pigeons_array.each do |bird|
+    organized_hash[bird] = {}
+  end
+
+  organized_hash.each do |bird, info|
+    ## Going back into the original hash to add to the new one
+    data.each do |category, attributes|
+      organized_hash[bird][category] = []
+      attributes.each do |stat, pigeons|
+        if pigeons.include?(bird)  
+          aspect = stat.to_s
+          organized_hash[bird][category] << aspect
+        end
+      end
+    end
+  end
+
+return organized_hash
 end
